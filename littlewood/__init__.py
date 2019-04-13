aa2371e46c013e85ec61dd52d19e7d67204582bc
Evan Berkowitz <git@evanberkowitz.com>
2019-04-13 17:39:56 +0200

    Update module __init__

--- littlewood/__init__.py
+++ littlewood/__init__.py
@@ -2,6 +2,7 @@ name="littlewood"
 
 import littlewood.coefficients
 import littlewood.io
+import littlewood.plot
 
 from .polynomials import polynomials
 from numpy.polynomial.polynomial import polyroots as roots
